40b000bab1b66c23e53ad90abe4f54f94b9b8d3a
Santiago <carsantiagosanchez@gmail.com>
2024-11-05 18:41:44 -0300

    Hicimos un contador de letras

---
+++ contador_letras.py
@@ -0,0 +1,13 @@
+# Ponemos como comentario lo que escribimos en el pseudocódigo.
+
+# Paso 1: Solicitar entrada al usuario.
+
+palabra_ingresada = input("Por favor, ingrese una palabra: ")
+
+# Paso 2: Contar la cantidad de letras.
+
+cantidad_letras = len(palabra_ingresada)
+
+# Paso 3: Mostrar al usuario el resultado. 
+
+print("La palabra ingresada tiene", cantidad_letras, "letras.")
